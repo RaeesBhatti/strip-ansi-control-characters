c373a15642434285df8c9a9fb9e14ba89642f4fa
Raees Iqbal <raees.bhatti@gmail.com>
2020-04-13 15:58:28 -0700

    Make piping possible

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 const replaceStream = require('replacestream');
 
 function StripAnsiCs(input) {
-  if (typeof input !== 'string' && (!input || typeof input !== 'object' || typeof input.pipe === 'function')) {
-    throw new Error('StripAnsiCsi: Invalid input provided for ANSI striping');
-  }
-
   const matcher = new RegExp('\\u001Bc*\\[*[0-9]*[HABCDEFGJKsu];*[0-9]*', 'gim');
 
   if (typeof input === 'string') {
     return input.replace(matcher, '');
   }
 
-  return input.pipe(replaceStream(matcher, ''));
+  return replaceStream(matcher, '');
 }
 
 module.exports = StripAnsiCs;
